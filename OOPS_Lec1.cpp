0f9e0bad5bd84ca24ef8d148747bf9f200301164
ayushhh321 <ayush56039@gmail.com>
2024-11-12 01:45:27 +0530

    theory

---
+++ OOPS_Lec1.cpp
@@ -0,0 +1,29 @@
+//OOPS 
+//Object - its is basically a real world entity which have some state and behavior. ex - laptop , mobile etc .its insatance of class
+
+//oops-techniqe jaha chize object ke around ghumti hai . we need object to porogram ko real world ke close laa skae kyuki meri program ki radeabilty , managebility badh jaye .
+
+//HERO- NAME ,HEALTH , LEVEL (properties)
+//hero - behaviour - attack() , defense()
+
+//int a , string str , char ch - yaha int , string , char type ka bana hai varaible
+
+//Hero ramesh - toh hero type ka ek variable bana diya which name is ramesh
+//so we can say that class is user defined data type hai custom data type hai which is hero and have some property.
+
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+  //properties
+  int health;
+}
+
+int main()
+{
+
+   
+
+
+}
